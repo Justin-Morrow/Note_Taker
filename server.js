b6266735e956cd8fdfe221e115e94f3ceec6efba
Justin Morrow <morrowjustin19@gmail.com>
2022-06-25 23:05:08 -0700

    updating server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+const app = express();
+const PORT = process.env.port || 3001;
+
+// Middleware for parsing JSON and urlencoded form data
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+// GET Route for feedback page
+require('./app/routes')(app);
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+});
